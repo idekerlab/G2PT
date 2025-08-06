1a818c81a741082e7d7389fab30c03e80a220ba5
dlsrnsi <lig0501!>
2025-08-05 23:41:32 -0700

    update readthedocs to add docstrings

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,9 @@ import os
 import sys
 import shutil
 
+# -- Mock imports ------------------------------------------------------------
+autodoc_mock_imports = ["torch", "pandas", "numpy", "scipy", "sklearn", "statsmodels", "networkx", "matplotlib", "seaborn", "tqdm", "mlflow", "dvc", "obonet"]
+
 # -- Copy Markdown files to source directory -------------------------------------
 
 def copy_markdown_files(app):
